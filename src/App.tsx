1be14c21755ecf081335d659a1899b7492aeefeb
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-07-21 13:28:35 +0530

    mapped dynamic data with data types

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,58 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+type Task = {
+  id: string;
+  content: string;
+};
+type Column = {
+  title: string;
+  tasks: Task[];
+};
+type BroadData = {
+  [key: string]: Column;
+};
+
+const initialData: BroadData = {
+  todo: {
+    title: "To Do",
+    tasks: [
+      { id: "1", content: "Learn React" },
+      { id: "2", content: "Setup Project" },
+    ],
+  },
+  inProgress: {
+    title: "In Progress",
+    tasks: [{ id: "3", content: "Write Code" }],
+  },
+  done: {
+    title: "Done",
+    tasks: [{ id: "4", content: "Project Idea" }],
+  },
+};
+
+const arrayedData = Object.entries(initialData);
+
+console.log(arrayedData);
+console.log(arrayedData.map(([itm]) => [itm]));
+
 function App() {
+  // const [columns, setcolumns] = useState(second)
   return (
     <>
       <div className="kanban-container">
-        <div className="kanban-column">
-          <h3>To Do</h3>
-          <div className="kanban-tasks">
-            <div className="kanban-task">Learn React</div>
+        {arrayedData.map(([columnIdx, column]) => (
+          <div className="kanban-column" key={columnIdx}>
+            <h3>{column.title}</h3>
+            <div className="kanban-tasks">
+              {column.tasks.map((task) => (
+                <div className="kanban-task" key={task.id}>
+                  {task.content}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="kanban-column">
-          <h3>In Progress</h3>
-          <div className="kanban-tasks">
-            <div className="kanban-task">Write Code</div>
-          </div>
-        </div>
-        <div className="kanban-column">
-          <h3>Done</h3>
-          <div className="kanban-tasks">
-            <div className="kanban-task">Project Idea</div>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
